66a2471ab49dcfc96dcb09578059e1e16e0c0333
Piet Delport <pjdelport@gmail.com>
2013-03-23 01:46:18 +0200

    Add management command: members_to_cidb_dataloadtool

---
+++ neo/management/commands/members_to_cidb_dataloadtool.py
@@ -0,0 +1,25 @@
+import sys
+from optparse import make_option
+
+from django.core.management.base import NoArgsCommand, CommandError
+
+from neo.models import Member, dataloadtool_export
+
+
+class Command(NoArgsCommand):
+    help = 'Export members as XML for the CIDB Data Load Tool.'
+
+    option_list = list(NoArgsCommand.option_list) + [
+        make_option('-f', '--file', dest='file', help='Output file (default: standard output)'),
+        make_option('-p', '--pretty-print', dest='pretty_print', action='store_true', default=False,
+                    help='Enable pretty-printing.'),
+    ]
+
+    def handle_noargs(self, filename=None, pretty_print=False, **options):
+        output = sys.stdout if filename is None else open(filename)
+        members = self.get_members()
+
+        dataloadtool_export(output, members, pretty_print=pretty_print)
+
+    def get_members(self):
+        return Member.objects.all()
