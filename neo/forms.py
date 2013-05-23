4b1a898ed27714b5ed4ab0155efc868765faa2b8
Rizmari Versfeld <rizmari@praekeltconsulting.com>
2013-05-23 17:09:37 +0200

    clean up join form patching

--- neo/forms.py
+++ neo/forms.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from django.forms.models import construct_instance
 
 from foundry.forms import JoinForm
@@ -5,51 +7,11 @@ from foundry.forms import JoinForm
 from neo.models import NeoProfile
 
 
-'''
-Patch the foundry JoinForm
-'''
-original_joinform_clean = JoinForm.clean
-original_joinform_save = JoinForm.save
+def patch_join_clean(original_clean):
 
-
-def clean_join_form(form):
-    cleaned_data = original_joinform_clean(form)
-    if 'password1' in form.cleaned_data:
-        opts = form._meta
-        # Update the model instance with cleaned_data.
-        member = construct_instance(form, form.instance, opts.fields, opts.exclude)
-        member.set_password(form.cleaned_data["password1"])
-        member.full_clean()
-        try:
-            form.neoprofile = member.neoprofile
-        except NeoProfile.DoesNotExist:
-            pass
-    return cleaned_data
-
-
-def save_join_form(form, commit=True):
-    instance = original_joinform_save(form, commit)
-    if hasattr(form, 'neoprofile') and form.neoprofile:
-        form.neoprofile.user = instance
-        form.neoprofile.save()
-    return instance
-
-
-JoinForm.clean = clean_join_form
-JoinForm.save = save_join_form
-
-
-'''
-If jmbo-registration is installed, patch that as well
-'''
-try:
-    from jmbo_registration.forms import JoinForm as RegJoinForm
-
-    original_regjoinform_clean = RegJoinForm.clean
-    original_regjoinform_save = RegJoinForm.save
-
-    def clean_reg_join_form(form):
-        cleaned_data = original_regjoinform_clean(form)
+    @wraps(original_clean)
+    def clean_join_form(form):
+        cleaned_data = original_clean(form)
         if 'password1' in form.cleaned_data:
             opts = form._meta
             # Update the model instance with cleaned_data.
@@ -62,15 +24,27 @@ try:
                 pass
         return cleaned_data
 
-    def save_reg_join_form(form, commit=True):
-        instance = original_regjoinform_save(form, commit)
+    return clean_join_form
+
+
+def patch_join_save(original_save):
+
+    @wraps(original_save)
+    def save_join_form(form, commit=True):
+        instance = original_save(form, commit)
         if hasattr(form, 'neoprofile') and form.neoprofile:
             form.neoprofile.user = instance
             form.neoprofile.save()
         return instance
 
-    RegJoinForm.clean = clean_reg_join_form
-    RegJoinForm.save = save_reg_join_form
+    return save_join_form
 
+
+JoinForm.clean = patch_join_clean(JoinForm.clean)
+JoinForm.save = patch_join_save(JoinForm.save)
+try:
+    from jmbo_registration.forms import JoinForm as RegJoinForm
+    RegJoinForm.clean = patch_join_clean(RegJoinForm.clean)
+    RegJoinForm.save = patch_join_save(RegJoinForm.save)
 except ImportError:
     pass
